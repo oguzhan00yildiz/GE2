276866e24355e481f24b2473536a7c2a44999a9f
oguzhan00yildiz <oguzhan00yildiz@gmail.com>
2024-08-24 22:10:37 +0300

    Character and basic Interaction controlls added

---
+++ Assets/PlayerInteractions.cs
@@ -0,0 +1,53 @@
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractions : MonoBehaviour
+{
+    private bool isShipDriving = false;
+    private  CharacterController characterController;
+    private ThirdPersonController thirdPersonController;
+    private Animator animator;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        thirdPersonController = GetComponent<ThirdPersonController>();
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)&&isShipDriving)
+        {
+            Debug.Log("Gemi kullaniliyor");
+            thirdPersonController.MoveSpeed = 0;
+            thirdPersonController.SprintSpeed = 0;
+            thirdPersonController.JumpHeight = 0;
+          thirdPersonController.CanMove = false;
+            
+
+        }
+
+        
+    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("ShipWheel"))
+        {
+            Debug.Log("Gemiyi Kullanmak icin E tusuna bas");
+            isShipDriving = true;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("ShipWheel"))
+        {
+            isShipDriving = false;
+        }
+    }
+}
